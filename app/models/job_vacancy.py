a476ee67ceec8bb6168671fce3c38cf6a8547049
Fouad Sfarijlani <fouad@Admins-MacBook-Pro.local>
2022-11-28 20:54:01 +0100

    Added one to one relationship between vacancy and vacancy hours

--- app/models/job_vacancy.py
+++ app/models/job_vacancy.py
@@ -7,9 +7,14 @@ class Vacancy(Base):
 
     id = Column(Integer, primary_key = True, index = True, nullable = False, autoincrement = True)
     title = Column(String, nullable=False)
+    hours_category_id = Column(ForeignKey("vacancy_hours.id"))
+
+    vacancy_hours = relationship("VacancyHours", back_populates="vacancy")
 
 class VacancyHours(Base):
     __tablename__ = "vacancy_hours"
 
     id = Column(Integer, primary_key = True, index = True, nullable = False, autoincrement = True)
     hours_category = Column(String, nullable = False)
+
+    vacancy = relationship("Vacancy", back_populates="vacancy_hours", uselist=False)
