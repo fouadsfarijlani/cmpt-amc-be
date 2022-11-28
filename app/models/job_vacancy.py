b2d398c48cc5175fc69b58282151e36a43fd0471
Fouad Sfarijlani <fouad@Admins-MacBook-Pro.local>
2022-11-28 20:44:43 +0100

    Added attributes to vacancies and made migrations

--- app/models/job_vacancy.py
+++ app/models/job_vacancy.py
@@ -2,8 +2,14 @@ from sqlalchemy import Integer, Column, ForeignKey, String
 from sqlalchemy.orm import relationship
 from app.database.database import Base
 
-class vacancy(Base):
+class Vacancy(Base):
     __tablename__ = "vacancy"
 
     id = Column(Integer, primary_key = True, index = True, nullable = False, autoincrement = True)
     title = Column(String, nullable=False)
+
+class VacancyHours(Base):
+    __tablename__ = "vacancy_hours"
+
+    id = Column(Integer, primary_key = True, index = True, nullable = False, autoincrement = True)
+    hours_category = Column(String, nullable = False)
